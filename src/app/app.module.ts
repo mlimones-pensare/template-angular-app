1e5c8f919bfdb425b658c66e84d909751794bac5
Manuel Limones <mlimones@pensare.mx>
2018-09-19 13:45:47 -0500

    Add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DashboardPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
