1e7fe27a6403e6bdc5a231260e3a2fb73f79c618
Manuel Limones <mlimones@pensare.mx>
2018-09-19 13:55:00 -0500

    Add authentication service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
+import { AuthGuard } from './auth.guard';
+
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
